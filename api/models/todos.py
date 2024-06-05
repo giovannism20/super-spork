7b8177306b9b44b1d6aebfa3b3a27d87ee3445ab
Giovanni Martins <giovannimartins2000@gmail.com>
2024-06-05 12:14:05 -0300

    databases

--- api/models/todos.py
+++ api/models/todos.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 engine = create_engine('sqlite:///todos.db')
@@ -9,7 +9,7 @@ Base = declarative_base()
 class Todo(Base):
     __tablename__ = "todo"
 
-    id = Column(Integer, primary_key=True, autoincrement=True,  nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     title = Column(String(50), nullable=False)
     description = Column(String(200), nullable=False)
 
